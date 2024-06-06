7a624d097130e2c106dcce517fec0d79f0a32425
sylvana <sylvanakafui@gmail.com>
2024-06-06 00:10:57 +0000

    Push shop_app on the repository

---
+++ partie1/A/AMAH-TCHOUTCHOUI Dédevi Silvia/shop_app/lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Colors.black;
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
